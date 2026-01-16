b3bb6f45d49aede4ed91de189ccb45a548b6a911
zmh <zouminhua@jemincare.com>
2026-01-16 15:30:19 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
-import { RouterProvider } from 'react-router';
-import router from './routes';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
+import router from "./routes";
 
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,20 @@
-import { createBrowserRouter, Navigate } from 'react-router';
-import Registry from '@/pages/signIn/page';
-import Home from '@/components/Home'; // 聊天主页面
-import AuthPage from '@/pages/login/page';
+import { createBrowserRouter } from "react-router";
+import Registry from "@/pages/signIn/page";
+import Home from "@/components/Home"; // 聊天主页面
+import AuthPage from "@/pages/login/page";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     // 根路径根据是否登录跳转，这里演示直接进入聊天或登录
     element: <Home />,
   },
   {
-    path: '/registry',
+    path: "/registry",
     element: <Registry onBack={() => {}} />,
   },
   {
-    path: '/login',
+    path: "/login",
     element: <AuthPage />,
   },
 ]);
